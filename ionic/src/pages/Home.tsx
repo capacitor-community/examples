6f0ed85ef4c74dd5c0dfd1cf2ed6e4e39a05eadb
Matt Netkow <matt.netkow@gmail.com>
2020-06-26 11:09:33 -0500

    add share api

--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -1,12 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
+import { shareSocialOutline } from 'ionicons/icons';
 
-import { Plugins, DeviceInfo } from '@capacitor/core';
+import { Plugins, DeviceInfo, Share } from '@capacitor/core';
 
 import './Home.css';
 
 const Home: React.FC = () => {
   const [deviceInfo, setDeviceInfo] = useState<DeviceInfo | null>(null);
+
+  const shareTweet = async () => {
+    const { Share } = Plugins;
+
+    const share = await Share.share({
+      title: 'See cool stuff',
+      text: 'Really awesome thing you need to see right meow',
+      url: 'http://ionicframework.com/',
+      dialogTitle: 'Share with buddies'
+    });
+  }
 
   useEffect(() => {
     const getDeviceInfo = async () => {
@@ -31,6 +43,10 @@ const Home: React.FC = () => {
             <IonTitle size="large">Capacitor Ionic Example</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonButton onClick={() => shareTweet()}>
+          <IonIcon slot="start" icon={shareSocialOutline} />
+          Share This
+        </IonButton>
         <div className="ion-padding">
           <h2>Device Info:</h2>
           <pre>
